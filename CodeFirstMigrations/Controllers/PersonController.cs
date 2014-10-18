0e198464a26099d5da043b7f2865fa3726e3c55e
Stino <Linksonder@gmail.com>
2014-10-18 17:02:41 +0200

    Full Solution
    Code First Migration Demo
    
    https://www.youtube.com/watch?v=pFPF87OgivU

---
+++ CodeFirstMigrations/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using CodeFirstMigrations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstMigrations.Controllers
+{
+    public class PersonController : Controller
+    {
+        /// <summary>
+        /// Mijn private field naar PersonContext
+        /// </summary>
+        private PersonContext context;
+
+        public PersonController()
+        {
+            //Ik initialiseer dit object in de constructor
+            context = new PersonContext();
+        }
+
+        public ActionResult Index()
+        {
+            //Ik heb alleen een view met een lijst van mensen
+            return View(context.People.ToList());
+        }
+
+    }
+}
---
+++ CodeFirstMigrations/Models/PersonContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstMigrations.Models
+{
+    /// <summary>
+    /// Mijn DbContext waar ik specifiek een connection string heb aangegeven
+    /// </summary>
+    public class PersonContext : DbContext
+    {
+        public PersonContext() //De constructor van PersonContext
+            : base("MyConnectionString") //De constructor van DbContext
+        {
+            //Ik geef aan de constructor van DbContext specifiek de naam mee van mijn connection string
+        }
+
+        /// <summary>
+        /// Mijn tabel van personen
+        /// </summary>
+        public DbSet<Person> People { get; set; }
+        
+    }
+}
